7983f595f85cbe97c3c5743166656d1afeb808a3
a.baronskiy <a.baronskiy@mail.ru>
2018-10-07 12:29:46 +0300

    done 1st hw

--- grep/grep.py
+++ grep/grep.py
@@ -1,7 +1,39 @@
-
 import argparse
 import sys
 import re
+
+def GetContextInfo(Nodes, i, before, after, ListOfMatchedEl):
+
+    StringNumbValue = []
+
+    LeftPart = i - before
+    if LeftPart < 0:
+        LeftPart = 0
+
+    for j in range (LeftPart, i):
+        new_dict = []
+        new_dict.append(j)
+        new_dict.append(Nodes[j])
+        StringNumbValue.append(tuple(new_dict))
+
+    ListOfMatchedEl.append(i)
+    m_el = []
+    m_el.append(i)
+    m_el.append(Nodes[i])
+    StringNumbValue.append(tuple(m_el))
+
+    RightPart = i + after + 1
+
+    if RightPart > len(Nodes):
+        RightPart = len(Nodes)
+
+    for j in range(i + 1, RightPart):
+        new_dict = []
+        new_dict.append(j)
+        new_dict.append(Nodes[j])
+        StringNumbValue.append(tuple(new_dict))
+
+    return StringNumbValue
 
 
 def output(line):
@@ -16,32 +48,69 @@ def count_grep(lines, regex):
     return count
 
 
-#workspace
 def grep(lines, params):
     regex_str = r""
+    for i in params.pattern:
+        pass
+        if i == '?':
+            regex_str += '.'
+        elif i == '*':
+            regex_str += '.' + i
+        else:
+            regex_str += i
 
     if(params.invert):
-        regex_str = r"(?!" + params.pattern + r")"
-    else:
-        regex_str = params.pattern
+        regex_str = r"(?!" + regex_str + r")"
 
     if(params.ignore_case):
         regex = re.compile(regex_str, re.IGNORECASE)
     else:
         regex = re.compile(regex_str)
-
     if(params.count):
         output(str(count_grep(lines, regex)))
         return
 
+    before_str = 0
+    after_str = 0
 
-    for line in lines:
-        line = line.rstrip()
+    if params.before_context:
+        before_str = params.before_context
+    elif params.context:
+        before_str = params.context
+    if params.after_context:
+        after_str = params.after_context
+    elif params.context:
+        after_str = params.context
+
+    ContextInfo = []
+
+    MatchedIndex = []
+
+    for i in range(len(lines)):
+        line = lines[i].rstrip()
+
         if params.invert:
             if regex.match(line):
-                output(line)
+                NewContexInfo = GetContextInfo(lines, i, before_str, after_str, MatchedIndex)
+                ContextInfo.extend(NewContexInfo)
+
+
         elif regex.search(line):
-            output(line)
+            NewContexInfo = GetContextInfo(lines, i, before_str, after_str, MatchedIndex)
+            ContextInfo.extend(NewContexInfo)
+
+    ContextInfo = sorted(set(ContextInfo), key = lambda x : x[0])
+    for line in ContextInfo:
+        if(params.line_number):
+            if line[0] in MatchedIndex:
+                output(str(line[0] + 1) + ":" + str(line[1]))
+            else:
+                output(str(line[0] + 1) + "-" + str(line[1]))
+        else:
+            output(line[1])
+
+
+
 
 
 def parse_args(args):
--- grep/tests/test_grep.py
+++ grep/tests/test_grep.py
@@ -1,7 +1,6 @@
 
 from unittest import TestCase
 
-# from grep import grep, parse_args
 import grep
 
 lst = []
@@ -10,9 +9,8 @@ lst = []
 def save_to_list(line):
     lst.append(line)
 
-# вместо grep.output
-grep.output = save_to_list
 
+grep.output = save_to_list
 
 class GrepBaseTest(TestCase):
 
@@ -23,35 +21,29 @@ class GrepBaseTest(TestCase):
         lst.clear()
 
     def test_base_scenario(self):
-        print("test_base_scenario-01")
         params = grep.parse_args(['aa'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['baab'])
 
     def test_base_scenario_multi(self):
-        print("test_base_scenario_multi")
         params = grep.parse_args(['b'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['baab', 'bbb'])
 
     def test_base_scenario_count(self):
-        print("test_base_scenario_count")
         params = grep.parse_args(['-c', 'a'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['1'])
 
     def test_base_scenario_invert(self):
-        print("test_base_scenario_invert")
         params = grep.parse_args(['-v', 'b'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['ccc', 'A'])
 
     def test_base_scenario_case(self):
-        print("test_base_scenario_case")
         params = grep.parse_args(['-i', 'a'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['baab', 'A'])
-
 
 class GrepPatternTest(TestCase):
 
@@ -62,43 +54,36 @@ class GrepPatternTest(TestCase):
         lst.clear()
 
     def test_question_base(self):
-        print("test_question_base")
         params = grep.parse_args(['?b'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['baab', 'abbb'])
 
     def test_question_start(self):
-        print("test_question_start")
         params = grep.parse_args(['?a'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['baab'])
 
     def test_queston_end(self):
-        print("test_queston_end")
         params = grep.parse_args(['c?'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, [])
 
-    def test_queston_double(self):
-        print("test_queston_double")
+    def test_queston_double(self): ## не сработали 2 знака вопроса
         params = grep.parse_args(['b??b'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['baab'])
 
     def test_queston_count(self):
-        print("test_queston_count")
         params = grep.parse_args(['???'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['baab', 'abbb'])
 
     def test_asterics(self):
-        print("test_asterics")
         params = grep.parse_args(['b*b'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['baab', 'abbb'])
 
     def test_asterics_all(self):
-        print("test_asterics_all")
         params = grep.parse_args(['***'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, self.lines)
@@ -112,32 +97,49 @@ class GrepContextTest(TestCase):
         lst.clear()
 
     def test_context_base(self):
-        print("test_context_base")
         params = grep.parse_args(['-C1','aa'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['vr', 'baab', 'abbb'])
 
     def test_context_intersection(self):
-        print("test_context_intersection")
         params = grep.parse_args(['-C1','ab'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['vr', 'baab', 'abbb', 'fc'])
 
-    def test_context_intersection_hard(self):
-        print("test_context_intersection_hard")
+    def test_context_intersection_hard(self): ##!!!
         params = grep.parse_args(['-C2','bbb'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, self.lines)
 
     def test_before(self):
-        print("test_before")
         params = grep.parse_args(['-B1','bbb'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['baab', 'abbb', 'fc', 'bbb'])
 
     def test_after(self):
-        print("test_after")
         params = grep.parse_args(['-A1','bbb'])
         grep.grep(self.lines, params)
         self.assertEqual(lst, ['abbb', 'fc', 'bbb', 'cc'])
 
+class GrepLineNumbersTest(TestCase):
+
+    lines = ['vr','baab', 'abbb', 'fc', 'bbb', 'cc']
+
+    def tearDown(self):
+        global lst
+        lst.clear()
+
+    def test_numbers_base(self):
+        params = grep.parse_args(['-n','ab'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['2:baab', '3:abbb'])
+
+    def test_numbers_context(self):
+        params = grep.parse_args(['-n', '-C1','aa'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['1-vr', '2:baab', '3-abbb'])
+
+    def test_numbers_context_question(self): ###
+        params = grep.parse_args(['-n', '-C1', '???'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['1-vr', '2:baab', '3:abbb', '4-fc', '5:bbb', '6-cc'])
